c71b4dd782c8f1e445361c2a0785a796de540ad2
Gavalan <gavalan@kkszki.hu>
2024-10-04 12:17:27 +0200

    minden

---
+++ WCF_elso/WCF_elso/IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.PeerResolvers;
+using System.Text;
+using WCF_elso_server.Models;
+using WCF_elso_server.Controllers;
+
+namespace WCF_elso_server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        //[WebGet(UriTemplate = "/GetEloado")]
+        List<Eloado> GetEloado();
+        [OperationContract]
+        //[WebGet(UriTemplate = "/GetEloadoName")]
+        string GetEloadoName();
+        [OperationContract]
+        //[WebGet(UriTemplate = "/GetZeneszam")]
+        Zeneszam GetZeneszam();
+        [OperationContract]
+        //[WebGet(UriTemplate = "/GetCD")]
+        CD GetCD();
+        [OperationContract]
+        string UjEloado(Eloado ujEloado);
+        [OperationContract]
+        string ModositEloado(Eloado eloado);
+        [OperationContract]
+        string TorolElado(Eloado elado);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+   
+    public class Zeneszam
+    {
+        [DataMember]
+        public int ZeneszamAz { get; set; }
+        [DataMember]
+        public string ZeneszamCim { get; set; }
+        [DataMember]
+        public int ZeneszamHossz { get; set; }
+    }
+
+    public class CD
+    {
+        [DataMember]
+        public int CdAz { get; set; }
+        [DataMember]
+        public string CdCim { get; set; }
+        [DataMember]
+        public string Kiado { get; set; }
+    }
+}
