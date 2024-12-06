3b2c76ff84a4be2d35e0ab0827f2cda1f2cc2ac9
GavalaN <gavalan@kkszki.hu>
2024-12-06 11:19:15 +0100

    backup

--- PatikaAPI/PatikaAPI/Controllers/KezelController.cs
+++ PatikaAPI/PatikaAPI/Controllers/KezelController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PatikaAPI.Models;
 
 namespace PatikaAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class KezelController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            using (var context = new PatikaContext())
+            {
+                try
+                {
+                    List<Kezel> kezelesek = context.Kezels.ToList();
+                    return Ok(kezelesek);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
 
+        [HttpPost("UjKezel")]
+        public IActionResult Post(Kezel UjKezel)
+        {
+            using (var context = new PatikaContext())
+            {
+                try
+                {
+                    context.Kezels.Include(k => k.Betegseg).Include(k => k.Gyogyszer).Add(UjKezel);
+                    context.SaveChanges();
+                    return Ok(context);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
     }
 }
